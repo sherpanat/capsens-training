082ff6dd5f6d1314dd6968425acda9bbd10823ef
Nathan Huberty <nathan.huberty@orange.fr>
2019-11-25 12:23:32 +0100

    Update CreateTransaction regarding comments.

--- app/transactions/users/create_transaction.rb
+++ app/transactions/users/create_transaction.rb
@@ -1,20 +1,20 @@
 module Users
   class CreateTransaction < ::BaseTransaction
     step :create_user
-    step :send_welcome_email
+    tee :send_welcome_email
 
     def create_user(input)
-      @user = User.create(input)
-      return Failure(error: @user.errors.full_messages.join(' | ')) if @user.errors.any?
-      Success(@user.attributes)
+      @user = User.new(input)
+      if @user.save
+        Success(@user.attributes)
+      else
+        Failure(error: @user.errors.full_messages.join(' | '))
+      end
     end
     
     def send_welcome_email(input)
       UserMailer.with(user: @user).welcome_email.deliver_now
-
       Success(@user.attributes)
-    rescue StandardError => exception
-      Failure(error: exception)
     end
   end
 end
