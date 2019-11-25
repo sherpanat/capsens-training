666422f377165a7cc5e4f5ee0f089f507f9ff9a3
Nathan Huberty <nathan.huberty@orange.fr>
2019-11-25 11:46:39 +0100

    Rename method and argument names.

--- app/transactions/users/create_transaction.rb
+++ app/transactions/users/create_transaction.rb
@@ -3,13 +3,13 @@ module Users
     step :create_user
     step :send_welcome_email
 
-    def create_user(input)
-      @user = User.create(input)
+    def create_user(attributes)
+      @user = User.create(attributes)
       return Failure(error: @user.errors.full_messages.join(' | ')) if @user.errors.any?
       Success(@user.attributes)
     end
     
-    def send_welcome_email(input)
+    def send_welcome_email(attributes)
       UserMailer.with(user: @user).welcome_email.deliver_now
 
       Success(@user.attributes)
