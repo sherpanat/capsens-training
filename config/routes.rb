f0be13473a52f06d91a2619f681db3f24d3f406d
Nathan Huberty <nathan.huberty@orange.fr>
2019-11-25 15:40:22 +0100

    WIP - Retrieve Devise User Controller.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'users/registrations' }
   root to: "pages#home"
 end
