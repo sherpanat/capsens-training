dcc90422be63b734d19c46e280999a3b237b5979
Nathan Huberty <nathan.huberty@orange.fr>
2019-11-26 15:09:40 +0100

    Refacto Users::CreateTransaction to with let and subject use.

--- spec/transactions/users/create_transaction_spec.rb
+++ spec/transactions/users/create_transaction_spec.rb
@@ -1,33 +1,18 @@
-RSpec.describe "User::CreateTransaction" do
+RSpec.describe Users::CreateTransaction do
   let!(:valid_user_attributes) { attributes_for(:user, email: 'nathan.patreau@capsens.fr') }
   let!(:invalid_user_attributes) { attributes_for(:user, first_name: '') }
 
-  context "Create a user" do
-    it "creates a user if valid attributes given" do
-      expect { ::Users::CreateTransaction.call(valid_user_attributes) }
-        .to change { User.count }.by(1)
-    end
-
-    it "does not create a user if invalid attributes given" do
-      expect { ::Users::CreateTransaction.call(invalid_user_attributes) }
-        .not_to change { User.count }
+  context "Create a user with valid attributes" do
+    it { expect { described_class.call(valid_user_attributes) }.to change { User.count }.by(1) }
+    it { expect { described_class.call(valid_user_attributes) }.to change { ActionMailer::Base.deliveries.count }.by(1) }
+    it "sends a welcoming email to user" do
+      expect_any_instance_of(UserMailer).to receive(:welcome_email)
+      described_class.call(valid_user_attributes)
     end
   end
 
-  context "Send welcome mail", type: :mailer do
-    it "sends an email" do
-      expect { ::Users::CreateTransaction.call(valid_user_attributes) }
-        .to change { ActionMailer::Base.deliveries.count }.by(1)
-    end
-
-    it "sends a welcoming email to user" do
-      expect_any_instance_of(UserMailer).to receive(:welcome_email)
-      ::Users::CreateTransaction.call(valid_user_attributes)
-    end
-
-    it "does not send an email not send" do
-      expect { ::Users::CreateTransaction.call(invalid_user_attributes) }
-        .not_to change { ActionMailer::Base.deliveries.count }
-    end
+  context "Create a user with invalid attributes" do
+    it { expect { described_class.call(invalid_user_attributes) }.not_to change { User.count } }
+    it { expect { described_class.call(invalid_user_attributes) }.not_to change { ActionMailer::Base.deliveries.count } }
   end
 end
