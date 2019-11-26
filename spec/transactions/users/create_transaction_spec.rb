702c123b415d133ba6141dbc8e8ad2def91c87d9
Nathan Huberty <nathan.huberty@orange.fr>
2019-11-26 11:09:06 +0100

    Refacto CreateTransaction spec to use .let!

--- spec/transactions/users/create_transaction_spec.rb
+++ spec/transactions/users/create_transaction_spec.rb
@@ -1,4 +1,7 @@
-RSpec.describe "User::CreateTransaction", type: :transaction do
+RSpec.describe "User::CreateTransaction" do
+  let!(:valid_user_attributes) { attributes_for(:user, email: 'nathan.patreau@capsens.fr') }
+  let!(:invalid_user_attributes) { attributes_for(:user, first_name: '') }
+
   context "Create a user" do
     it "creates a user if valid attributes given" do
       expect { ::Users::CreateTransaction.call(valid_user_attributes) }
@@ -6,7 +9,6 @@ RSpec.describe "User::CreateTransaction", type: :transaction do
     end
 
     it "does not create a user if invalid attributes given" do
-      create(:user, email: 'nathan.patreau@capsens.fr')
       expect { ::Users::CreateTransaction.call(invalid_user_attributes) }
         .not_to change { User.count }
     end
@@ -24,28 +26,8 @@ RSpec.describe "User::CreateTransaction", type: :transaction do
     end
 
     it "does not send an email not send" do
-      create(:user, email: 'nathan.patreau@capsens.fr')
       expect { ::Users::CreateTransaction.call(invalid_user_attributes) }
         .not_to change { ActionMailer::Base.deliveries.count }
     end
-  end
-
-  def valid_user_attributes
-    {
-      first_name: 'Nathan',
-      last_name: 'Patreau',
-      email: 'nathan.patreau@capsens.fr',
-      birthdate: '08/09/1998',
-      password: '12345678'
-    }
-  end
-
-  def invalid_user_attributes
-    {
-      last_name: 'Patreau',
-      email: 'nathan.patreau@capsens.fr',
-      birthdate: '08/09/1998',
-      password: '12345678'
-    }
   end
 end
