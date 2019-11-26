36e45cea80834698c9fa73b8e8a96305a096e3c8
Nathan Huberty <nathan.huberty@orange.fr>
2019-11-26 16:08:15 +0100

    Refacto tests using subject.

--- spec/transactions/users/create_transaction_spec.rb
+++ spec/transactions/users/create_transaction_spec.rb
@@ -1,18 +1,19 @@
 RSpec.describe Users::CreateTransaction do
-  let!(:valid_user_attributes) { attributes_for(:user, email: 'nathan.patreau@capsens.fr') }
-  let!(:invalid_user_attributes) { attributes_for(:user, first_name: '') }
+  subject { described_class.call(user_attributes) }
 
   context "Create a user with valid attributes" do
-    it { expect { described_class.call(valid_user_attributes) }.to change { User.count }.by(1) }
-    it { expect { described_class.call(valid_user_attributes) }.to change { ActionMailer::Base.deliveries.count }.by(1) }
+    let(:user_attributes) { attributes_for(:user, email: 'nathan.patreau@capsens.fr') }
+    it { expect { subject }.to change { User.count }.by(1) }
+    it { expect { subject }.to change { ActionMailer::Base.deliveries.count }.by(1) }
     it "sends a welcoming email to user" do
       expect_any_instance_of(UserMailer).to receive(:welcome_email)
-      described_class.call(valid_user_attributes)
+      subject
     end
   end
 
   context "Create a user with invalid attributes" do
-    it { expect { described_class.call(invalid_user_attributes) }.not_to change { User.count } }
-    it { expect { described_class.call(invalid_user_attributes) }.not_to change { ActionMailer::Base.deliveries.count } }
+    let(:user_attributes) { attributes_for(:user, first_name: '') }
+    it { expect { subject }.not_to change { User.count } }
+    it { expect { subject }.not_to change { ActionMailer::Base.deliveries.count } }
   end
 end
