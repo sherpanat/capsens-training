6a18b27f41ef941aee3e5389cc02b5e3a72ba9b3
Nathan Huberty <nathan.huberty@orange.fr>
2019-11-27 16:13:33 +0100

    Make CreateTransaction return user instance.

--- app/transactions/users/create_transaction.rb
+++ app/transactions/users/create_transaction.rb
@@ -6,12 +6,12 @@ module Users
     def create_user(attributes)
       @user = User.new(attributes)
       if @user.save
-        Success(@user.attributes)
+        Success(@user)
       else
-        Failure(error: @user.errors.full_messages.join(' | '))
+        Failure(error: @user.errors.full_messages.join(' | '), user: @user)
       end
     end
-    
+
     def send_welcome_email(attributes)
       UserMailer.with(user: @user).welcome_email.deliver_now
     end
