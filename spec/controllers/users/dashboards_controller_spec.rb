c3b823b8721fd7461755abaef8ae22357ef6305d
Nathan Huberty <nathan.huberty@gmail.com>
2019-12-02 11:48:21 +0100

    Create Users::DashboardsController specs.

--- spec/controllers/users/dashboards_controller_spec.rb
+++ spec/controllers/users/dashboards_controller_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
-RSpec.describe My::DashboardsController, type: :controller do
+RSpec.describe Users::DashboardsController, type: :controller do
+  describe "GET show" do
+    context "if user logged in" do
+      it "returns User info" do
+        sign_in create(:user)
+        get :show
+        expect(response.status).to eq(200)
+        expect(response).to render_template :show
+      end
+    end
 
+    context "if no user logged in" do
+      it "redirects to sign in page" do
+        get :show
+        expect(response.status).to eq(302)
+        expect(response).to redirect_to user_session_path
+      end
+    end
+  end
 end
