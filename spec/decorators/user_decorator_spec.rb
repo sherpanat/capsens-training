ab82cbec6fe8ebdf10fd1e34b66958eecd6470e2
Nathan Huberty <nathan.huberty@gmail.com>
2019-12-03 10:06:38 +0100

    Test User Decorator.

--- spec/decorators/user_decorator_spec.rb
+++ spec/decorators/user_decorator_spec.rb
@@ -1,4 +1,6 @@
-require 'rails_helper'
-
 RSpec.describe UserDecorator do
+  let(:user) { create(:user).decorate }
+  describe "full_name" do
+    it { expect(user.full_name).to eq("#{user.first_name} #{user.last_name}") }
+  end
 end
