50fabca18470c7a5eef24fe25cc78e8231d1ed8d
Nathan Huberty <nathan.huberty@gmail.com>
2019-12-04 18:26:00 +0100

    Attach pictures to Project.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_03_153221) do
+ActiveRecord::Schema.define(version: 2019_12_04_085824) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,6 +60,8 @@ ActiveRecord::Schema.define(version: 2019_12_03_153221) do
     t.bigint "category_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "thumbnail_data"
+    t.text "landscape_data"
     t.index ["category_id"], name: "index_projects_on_category_id"
   end
 
