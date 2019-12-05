91457a1918da8216a10b327b56cd5ca35fb8b55a
Nathan Huberty <nathan.huberty@gmail.com>
2019-12-05 12:19:32 +0100

    Make category_id optional for a project.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_04_085824) do
+ActiveRecord::Schema.define(version: 2019_12_05_111228) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,7 +57,7 @@ ActiveRecord::Schema.define(version: 2019_12_04_085824) do
     t.string "short_description"
     t.text "long_description"
     t.integer "target_amount", default: 0
-    t.bigint "category_id", null: false
+    t.bigint "category_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.text "thumbnail_data"
