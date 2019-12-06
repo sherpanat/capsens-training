e1521bac4a4d32ff125b752fbc052c4078d50bd3
Nathan Huberty <nathan.huberty@gmail.com>
2019-12-06 12:25:10 +0100

    Fix typo.

--- app/models/contribution.rb
+++ app/models/contribution.rb
@@ -2,5 +2,5 @@ class Contribution < ApplicationRecord
   belongs_to :user
   belongs_to :project
 
-  validates :contribution, presence: true
+  validates :amount, presence: true
 end
--- db/seeds/contributions.seeds.rb
+++ db/seeds/contributions.seeds.rb
@@ -2,14 +2,14 @@ after :users, :projects, :counterparts do
   puts "Creating 1 Contribution by each 2 Users to 1 project..."
   project = Project.find_by(name: "1001pact")
   user = User.find_by(email: "nathan.huberty@lita.co")
-  Contribution.find_or_create_by(
+  Contribution.create(
     project: project,
     user: user,
     amount: project.target_amount / 20
   )
   puts "... 1 created..."
   user = User.find_by(email: "abel.ilito@lita.co")
-  Contribution.find_or_create_by(
+  Contribution.create(
     project: project,
     user: user,
     amount: project.target_amount / 5
