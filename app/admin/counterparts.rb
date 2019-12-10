e1daf3c6d5372a1fba239145f65dbab6e1824572
Nathan Huberty <nathan.huberty@gmail.com>
2019-12-10 18:18:38 +0100

    Add Admin Counterpart form and custom redirections.

---
+++ app/admin/counterparts.rb
@@ -0,0 +1,42 @@
+ActiveAdmin.register Counterpart do
+  actions :all, except: [:index, :show]
+  permit_params :description, :threshold, :stock, :project_id
+
+  form do |f|
+    f.inputs do
+      f.input :description
+      f.input :threshold
+      f.input :stock
+      f.input :project, as: :select
+    end
+    f.actions do
+      f.action :submit
+    end
+  end
+
+  controller do
+    def create
+      super do
+        redirect_to_project_show and return if resource.valid?
+      end
+    end
+
+    def update
+      super do
+        redirect_to_project_show and return if resource.valid?
+      end
+    end
+
+    def destroy
+      super do
+        redirect_to_project_show and return if resource.valid?
+      end
+    end
+
+    private
+    
+    def redirect_to_project_show
+      redirect_to admin_project_path(resource.project)
+    end
+  end
+end
