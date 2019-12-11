0da1a8ebf411299849135d99ee141719e5cb8358
Nathan Huberty <nathan.huberty@gmail.com>
2019-12-11 11:42:51 +0100

    Install and setup aasm gem for Project.

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,12 @@
 class Project < ApplicationRecord
+  include AASM
+  aasm whiny_transitions: false
+
+  aasm do
+    state :draft, initial: true
+    state :upcomming, :ongoing, :success, :failure
+  end
+
   include ImageUploader::Attachment.new(:landscape)
   include ImageUploader::Attachment.new(:thumbnail)
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_06_160657) do
+ActiveRecord::Schema.define(version: 2019_12_11_103638) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -84,6 +84,7 @@ ActiveRecord::Schema.define(version: 2019_12_06_160657) do
     t.datetime "updated_at", precision: 6, null: false
     t.text "thumbnail_data"
     t.text "landscape_data"
+    t.string "aasm_state"
     t.index ["category_id"], name: "index_projects_on_category_id"
   end
 
