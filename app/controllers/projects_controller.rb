1ea0fc242289f77f57635230512f5ae68a77160b
Nathan Huberty <nathan.huberty@gmail.com>
2019-12-13 16:30:27 +0100

    Refacto.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,7 +2,7 @@ class ProjectsController < ApplicationController
   before_action :authenticate_admin_user!, only: :show, if: :draft_project?
 
   def show
-    @project = @project || Project.find(params[:id])
+    @project ||= Project.find(params[:id])
   end
 
   private
