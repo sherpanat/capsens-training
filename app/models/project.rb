d46d00d3cec4c4d3398ed8ec0fd2390065c8b2d4
Nathan Huberty <nathan.huberty@gmail.com>
2019-12-16 10:55:15 +0100

    Add and test visibles project scope.

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,6 +3,10 @@ class Project < ApplicationRecord
   include ImageUploader::Attachment.new(:thumbnail)
   include Aasm::Project
 
+  scope :visibles, ->() do
+    where(aasm_state: "ongoing").or(self.where(aasm_state: "upcoming")).or(self.where(aasm_state: "success"))
+  end
+
   belongs_to :category, optional: true
   has_many :contributions
   has_many :contributors, through: :contributions, source: :user
