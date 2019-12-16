38ee24d5edb4d83b273638cf1cea69281695db78
Nathan Huberty <nathan.huberty@gmail.com>
2019-12-16 11:24:47 +0100

    End project index.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,6 +1,10 @@
 class ProjectsController < ApplicationController
   before_action :authenticate_admin_user!, only: :show, if: :draft_project?
 
+  def index
+    @projects = Project.visibles
+  end
+
   def show
     @project ||= Project.find(params[:id])
   end
