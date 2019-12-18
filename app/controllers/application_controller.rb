01077aba0c5c9fc46233457e9083039a39b7cc96
Nathan Huberty <nathan.huberty@gmail.com>
2019-12-18 15:49:49 +0100

    Remove before_actions in projects_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,4 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :birthdate])
   end
-
-  def render_not_found
-    render :file => "#{Rails.root}/public/404.html",  :status => 404
-  end
 end
