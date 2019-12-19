a319c7a99ea2464866fc3d32fcd5655855fbb83e
Nathan Huberty <nathan.huberty@gmail.com>
2019-12-19 11:39:49 +0100

    Add custom validation sufficient_amount_for_counterpart.

--- app/models/contribution.rb
+++ app/models/contribution.rb
@@ -4,4 +4,13 @@ class Contribution < ApplicationRecord
   belongs_to :counterpart, optional: true
 
   validates :amount, presence: true
+  validate :sufficient_amount_for_counterpart, if: :counterpart
+
+  private
+
+  def sufficient_amount_for_counterpart
+    if amount < counterpart.threshold
+      errors.add(:counterpart, "Montant trop faible")
+    end
+  end
 end
