305de71d009c99f835b4d907b4ebe76992901975
Nathan Huberty <nathan.huberty@gmail.com>
2019-12-19 17:02:26 +0100

    Hide credentials.

--- config/initializers/mango_pay.rb
+++ config/initializers/mango_pay.rb
@@ -2,8 +2,8 @@ require "mangopay"
 
 MangoPay.configure do |c|
   c.preproduction = true
-  c.client_id = 'test-capsens'
-  c.client_apiKey = 'YOEa3orzMhcqNjxWN41aU9BPA5Jqhqr4ZmnfkQmq33DBkqGpHg'
+  c.client_id = ENV['MANGOPAY_CLIENT_ID']
+  c.client_apiKey = ENV['MANGOPAY_CLIENT_API_KEY]'
   c.log_file = File.join('log', 'mangopay.log')
   c.http_timeout = 10_000
 end
