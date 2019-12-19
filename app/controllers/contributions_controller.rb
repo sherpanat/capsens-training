7d448a92aafff2ad9e4df82dfddcafa5ac9e4a59
Nathan Huberty <nathan.huberty@gmail.com>
2019-12-19 11:40:09 +0100

    End ContributionsController create.

--- app/controllers/contributions_controller.rb
+++ app/controllers/contributions_controller.rb
@@ -1,18 +1,34 @@
 class ContributionsController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_project, only: :new
 
   def new
-    @contribution = Contribution.new(user: current_user, project: @project)
-    @counterparts = @project.counterparts
+    @contribution = Contribution.new(user: current_user, project: set_project)
+    set_counterparts
   end
   
   def create
+    contribution = Contribution.new(contribution_params.merge(project_id: params[:project_id], user: current_user))
+    if contribution.save
+      redirect_to users_dashboards_path
+    else
+      @contribution = contribution
+      set_project
+      set_counterparts
+      render :new
+    end
   end
 
   private
 
   def set_project
     @project = Project.find(params[:project_id])
+  end
+
+  def set_counterparts
+    @counterparts = @project.counterparts
+  end
+
+  def contribution_params
+    params.require(:contribution).permit(:amount, :counterpart_id)
   end
 end
