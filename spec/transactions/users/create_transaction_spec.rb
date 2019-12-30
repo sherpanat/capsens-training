be8e393b8bd5e25bddc10eac4311f3fc29d9d899
Nathan Huberty <nathan.huberty@gmail.com>
2019-12-30 17:59:13 +0100

    Test Mangopay User creation.

--- spec/transactions/users/create_transaction_spec.rb
+++ spec/transactions/users/create_transaction_spec.rb
@@ -3,6 +3,16 @@ RSpec.describe Users::CreateTransaction do
 
   context "Create a user with valid attributes" do
     let(:user_attributes) { attributes_for(:user, email: 'nathan.patreau@capsens.fr') }
+    before "Create a MangoPay::NaturalUser" do
+      expect(MangoPay::NaturalUser).to receive(:create).with(
+        FirstName: user_attributes[:first_name],
+        LastName: user_attributes[:last_name],
+        Birthday: user_attributes[:birthdate].to_time.to_i,
+        Nationality: 'FR',
+        CountryOfResidence: 'FR',
+        Email: user_attributes[:email]
+      )
+    end
     it { expect { subject }.to change { User.count }.by(1) }
     it { expect { subject }.to change { ActionMailer::Base.deliveries.count }.by(1) }
     it "sends a welcoming email to user" do
@@ -13,6 +23,9 @@ RSpec.describe Users::CreateTransaction do
 
   context "Create a user with invalid attributes" do
     let(:user_attributes) { attributes_for(:user, first_name: '') }
+    before "Does not create a MangoPay::NaturalUser" do
+      expect(MangoPay::NaturalUser).not_to receive(:create)
+    end
     it { expect { subject }.not_to change { User.count } }
     it { expect { subject }.not_to change { ActionMailer::Base.deliveries.count } }
   end
