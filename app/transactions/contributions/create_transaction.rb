cac344629d4d922af37e1ba77c2cb382125407db
Nathan Huberty <nathan.huberty@gmail.com>
2020-01-07 16:34:58 +0100

    Move counterpart validation to contribution create transaction

--- app/transactions/contributions/create_transaction.rb
+++ app/transactions/contributions/create_transaction.rb
@@ -1,11 +1,22 @@
 module Contributions
   class CreateTransaction < ::BaseTransaction
+    step :validate_counterpart
     step :create_contribution
 
-    def create_contribution(attributes)
+    def validate_counterpart(attributes)
       @contribution = Contribution.new(attributes)
+      return Success(contribution: @contribution) unless @contribution.counterpart
+      if @contribution.amount < @contribution.counterpart.threshold
+        @contribution.errors.add(:counterpart, :amount_lower_than_threshold)
+        Failure(error: @contribution.errors.full_messages.join(' | '), contribution: @contribution)
+      else
+        Success(contribution: @contribution)
+      end
+    end
+
+    def create_contribution
       if @contribution.save
-        Success(@contribution)
+        Success(contribution: @contribution)
       else
         Failure(error: @contribution.errors.full_messages.join(' | '), contribution: @contribution)
       end
--- spec/transactions/contributions/create_transaction_spec.rb
+++ spec/transactions/contributions/create_transaction_spec.rb
@@ -4,16 +4,34 @@ RSpec.describe Contributions::CreateTransaction do
   context "Create a contribution with valid attributes" do
     let(:user) { create(:user) }
     let(:project) { create(:project) }
-    let(:counterpart) { create(:counterpart, project: project, threshold: 20) }
-    let(:contribution_attributes) { attributes_for(:contribution, amount: 50).merge(user_id: user.id, project_id: project.id, counterpart_id: counterpart.id) }
-    it { expect { subject }.to change { Contribution.count }.by(1) }
-    it "Create contribution with counterpart" do
-      expect(subject.success.counterpart).to eq(counterpart)
+
+    describe "with a relevant conterpart" do
+      let(:counterpart) { create(:counterpart, project: project, threshold: 20) }
+      let(:contribution_attributes) { attributes_for(:contribution, amount: 20).merge(user_id: user.id, project_id: project.id, counterpart_id: counterpart.id) }
+      it { expect { subject }.to change { Contribution.count }.by(1) }
+      it "creates contribution with counterpart" do
+        expect(subject.success[:contribution].counterpart).to eq(counterpart)
+      end
+    end
+
+    describe "without any counterpart" do
+      let(:contribution_attributes) { attributes_for(:contribution, amount: 20).merge(project_id: project.id, user_id: user.id) }
+      it { expect { subject }.to change { Contribution.count }.by(1) }
     end
   end
 
-  context "Create a user with invalid attributes" do
-    let(:contribution_attributes) { attributes_for(:contribution) }
-    it { expect { subject }.not_to change { Contribution.count } }
+  context "Create a contribution with invalid attributes" do
+    describe "without any counterpart" do
+      let(:contribution_attributes) { attributes_for(:contribution) }
+      it { expect { subject }.not_to change { Contribution.count } }
+    end
+    
+    describe "with counterpart out of price range" do
+      let(:user) { create(:user) }
+      let(:project) { create(:project) }
+      let(:counterpart) { create(:counterpart, project: project, threshold: 20) }
+      let(:contribution_attributes) { attributes_for(:contribution, amount: 10).merge(project_id: project.id, user_id: user.id, counterpart_id: counterpart.id) }
+      it { expect { subject }.not_to change { Contribution.count } }
+    end
   end
 end
