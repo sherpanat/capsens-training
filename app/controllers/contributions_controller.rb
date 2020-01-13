2d623dade727cb9f4f8c44673fdfcaf84f9507c8
Nathan Huberty <nathan.huberty@gmail.com>
2020-01-13 11:19:52 +0100

    Refacto Contribution controller to use a project method.

--- app/controllers/contributions_controller.rb
+++ app/controllers/contributions_controller.rb
@@ -2,12 +2,12 @@ class ContributionsController < ApplicationController
   before_action :authenticate_user!
 
   def new
-    @project = get_project
+    @project = project
     @contribution = Contribution.new(user: current_user, project: @project)
   end
   
   def create
-    @project = get_project
+    @project = project
     result = Contributions::CreateTransaction.call(contribution_params.merge(project: @project, user: current_user))
     if result.success
       redirect_to users_dashboards_path
@@ -19,12 +19,13 @@ class ContributionsController < ApplicationController
   end
 
   private
-
-  def get_project
-    Project.find(params[:project_id]).decorate
-  end
-
+  
   def contribution_params
     params.require(:contribution).permit(:amount, :counterpart_id)
   end
+
+  def project
+    @decorated_project ||= Project.find(params[:project_id]).decorate
+  end
+  helper_method :project
 end
