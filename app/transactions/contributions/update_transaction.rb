74326a15619b4a1741e1cec73354359e1e46108a
Nathan Huberty <nathan.huberty@gmail.com>
2020-01-13 14:07:43 +0100

    WIP - Update update_transaction contribution.

--- app/transactions/contributions/update_transaction.rb
+++ app/transactions/contributions/update_transaction.rb
@@ -4,36 +4,35 @@ module Contributions
     step :transfer_from_user_to_contribution_wallet
 
     def create_user_contribution_wallet(attributes)
-      @contribution = Contribution.find(attributes['id'])
-      @project = Project.find(attributes['project_id'])
-      @user = User.find(attributes['user_id'])
+      contribution = Contribution.find(attributes['id'])
+      project = Project.find(attributes['project_id'])
+      user = User.find(attributes['user_id'])
       contribution_wallet = MangoPay::Wallet.create(
-        Owners: [@user.mangopay_id],
-        Description: "Contribution's wallet for #{@user.first_name} #{@user.last_name} contribution on #{@project.name} project.",
+        Owners: [user.mangopay_id],
+        Description: "Contribution's wallet for #{user.first_name} #{user.last_name} contribution on #{project.name} project.",
         Currency: "EUR"
       )
-      @contribution.update!(wallet_id: contribution_wallet['Id'])
-      p contribution_wallet
-      Success(contribution: @contribution, mangopay_wallet: contribution_wallet)
+      contribution.update!(wallet_id: contribution_wallet['Id'])
+      Success(contribution)
     end
 
-    def transfer_from_user_to_contribution_wallet
-      transfert = MangoPay::Transfer.create(
-        AuthorId: @user.mangopay_id,
-        CreditedUserId: @user.mangopay_id,
+    def transfer_from_user_to_contribution_wallet(contribution)
+      transfer = MangoPay::Transfer.create(
+        AuthorId: contribution.user.mangopay_id,
+        CreditedUserId: contribution.user.mangopay_id,
         DebitedFunds: {
           Currency: "EUR",
-          Amount: @contribution.amount * 100
+          Amount: contribution.amount * 100
         },
         Fees: {
           Currency: "EUR",
           Amount: 0
         },
-        DebitedWalletId: @user.wallet_id,
-        CreditedWalletId: @contribution.wallet_id
+        DebitedWalletId: contribution.user.wallet_id,
+        CreditedWalletId: contribution.wallet_id
       )
-      p transfert
-      Success(contribution: @contribution, transfert_to_contribution_wallet: transfert)
+      contribution.update!(transfer_id: transfer['Id'])
+      Success(contribution)
     end
   end
 end
