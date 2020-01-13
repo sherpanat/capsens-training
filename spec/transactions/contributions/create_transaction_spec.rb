1e24dc576a8f50ed14e9654290066a1e4125f639
Nathan Huberty <nathan.huberty@gmail.com>
2020-01-13 09:53:01 +0100

    Make Successfull transaction return instance instead of hash.

--- spec/transactions/contributions/create_transaction_spec.rb
+++ spec/transactions/contributions/create_transaction_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe Contributions::CreateTransaction do
       let(:contribution_attributes) { attributes_for(:contribution, amount: 20).merge(user_id: user.id, project_id: project.id, counterpart_id: counterpart.id) }
       it { expect { subject }.to change { Contribution.count }.by(1) }
       it "creates contribution with counterpart" do
-        expect(subject.success[:contribution].counterpart).to eq(counterpart)
+        expect(subject.success.counterpart).to eq(counterpart)
       end
     end
 
