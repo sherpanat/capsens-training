95e49d278d3b7175d0296f4f5221c0edb70c531d
Nathan Huberty <nathan.huberty@gmail.com>
2020-01-14 11:34:23 +0100

    Remove useless comment.

--- spec/transactions/contributions/update_transaction_spec.rb
+++ spec/transactions/contributions/update_transaction_spec.rb
@@ -26,22 +26,6 @@ RSpec.describe Contributions::UpdateTransaction do
         CreditedWalletId: "1"
       ).and_return("Balance"=> { "Currency" => "EUR", "Amount" => 0 }, "Id" => "2")
     end
-      # {"Id"=>"73418415", "Tag"=>nil,
-      # "CreationDate"=>1578674573,
-      # "AuthorId"=>"73348151",
-      # "CreditedUserId"=>"73348151",
-      # "DebitedFunds"=>{"Currency"=>"EUR", "Amount"=>18},
-      # "CreditedFunds"=>{"Currency"=>"EUR", "Amount"=>18},
-      # "Fees"=>{"Currency"=>"EUR", "Amount"=>0},
-      # "Status"=>"FAILED",
-      # "ResultCode"=>"001001",
-      # "ResultMessage"=>"Unsufficient wallet balance",
-      # "ExecutionDate"=>nil,
-      # "Type"=>"TRANSFER",
-      # "Nature"=>"REGULAR",
-      # "DebitedWalletId"=>"73348152",
-      # "CreditedWalletId"=>"73347199"
-      # }})
     let(:contribution) { create(:contribution, amount: 20) }
     let(:contribution_attributes) { contribution.attributes }
     it "sets wallet_id" do
