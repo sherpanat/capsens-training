89cfd0652381e20a431a4f866518e793ca50886d
Nathan Huberty <nathan.huberty@gmail.com>
2020-01-22 09:32:31 +0100

    Delete comment.

--- spec/transactions/projects/end_collect_transaction_spec.rb
+++ spec/transactions/projects/end_collect_transaction_spec.rb
@@ -43,34 +43,5 @@ RSpec.describe Projects::EndCollectTransaction do
         expect(result.first["Id"]).to eq("1")
       end
     end
-    
-    #   before "creates Mangopay::Transfer from contribution's wallet to project's wallet" do
-    #     expect(MangoPay::Transfer).to receive(:create).with(
-    #       AuthorId: contribution.user.mangopay_id,
-    #       CreditedUserId: contribution.user.mangopay_id,
-    #       DebitedFunds: {
-    #         Currency: "EUR",
-    #         Amount: contribution.amount * 100
-    #       },
-    #       Fees: {
-    #         Currency: "EUR",
-    #         Amount: 0
-    #       },
-    #       DebitedWalletId: contribution.wallet_id,
-    #       CreditedWalletId: contribution.project.wallet_id
-    #     ).and_return("Id" => "1")
-    #   end
-    #   let(:contribution) { create(:contribution_payed, amount: 100, project: project) }
-
-    #   it "sets transfer_to_project_wallet_id" do
-    #     project.prepare!
-    #     project.publish!
-    #     project.end_collect!
-    #     contribution.pay!
-    #     result = subject.success
-    #     expect(result).to be_an_instance_of(Array)
-    #     expect(result.first.transfer_to_project_wallet_id).to eq("1")
-    #   end
-    # end
   end
 end
